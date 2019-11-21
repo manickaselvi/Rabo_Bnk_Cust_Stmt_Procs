82c06db0e6688c7accdc239f2684315511f3c3df
manickaselvi <57222412+manickaselvi@users.noreply.github.com>
2019-11-21 17:55:11 +0530

    Main class to execute Rabo bank statement processor

---
+++ src/main/java/com/rabobank/RaboBnkCustStmtProcessorApplication.java
@@ -0,0 +1,18 @@
+package com.rabobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author manickaselvi m
+ *
+ */
+@SpringBootApplication	
+public class RaboBnkCustStmtProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaboBnkCustStmtProcessorApplication.class, args);
+	}
+
+}
