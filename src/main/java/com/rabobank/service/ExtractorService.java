959a5ccc85a27a90746e80f7df46f9b6751377c9
manickaselvi <57222412+manickaselvi@users.noreply.github.com>
2019-12-03 19:21:58 +0530

    Updated ExtractorService.java

--- src/main/java/com/rabobank/service/ExtractorService.java
+++ src/main/java/com/rabobank/service/ExtractorService.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-
+import com.rabobank.exceptions.RaboBankStmtProcessException;
 import com.rabobank.model.Record;
 
 /**
@@ -14,8 +14,8 @@ import com.rabobank.model.Record;
 @Service
 public interface ExtractorService {
 
-	List<Record> extractStatmentFromCSV(File file) throws Exception;
+	List<Record> extractStatmentFromCSV(File file) throws RaboBankStmtProcessException;
 	
-	List<Record> extractStatmentFromXML(File file) throws Exception;
+	List<Record> extractStatmentFromXML(File file) throws RaboBankStmtProcessException;
 	
 }
