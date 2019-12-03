da7204b13a591a9bd91f31ee127f6545984cb7fb
manickaselvi <57222412+manickaselvi@users.noreply.github.com>
2019-12-03 20:32:36 +0530

    Test cases added for Service Implementations

--- src/test/java/com/rabobank/service/ExtractorServiceImplTest.java
+++ src/test/java/com/rabobank/service/ExtractorServiceImplTest.java
@@ -1,1 +1,60 @@
+package com.rabobank.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rabobank.exceptions.RaboBankStmtProcessException;
+import com.rabobank.model.Record;
+
+@SpringBootTest
+public class ExtractorServiceImplTest {
+	
+	@Value("${rabo.bank.process.statement.file.name}")
+	private String processStatementFileName;
+	
+	/**
+	 * Scenario : Positive 
+	 * scenario : Processing the input CSV file and extracting
+	 * values as POJO object for validation process
+	 * @throws IOException 
+	 * @throws RaboBankStmtProcessException 
+	 */
+	@Test
+	public void extractStatmentFromCSVTestCase() throws IOException, RaboBankStmtProcessException {
+		ExtractorService extractorServiceImpl = new ExtractorServiceImpl();
+		File inputFile = new File(processStatementFileName + ".csv");
+		int totalLineInInputCSV = (int) Files.lines(Paths.get(inputFile.getName())).count();
+		List<Record> extractedRecords = extractorServiceImpl.extractStatmentFromCSV(inputFile);
+		assertEquals(totalLineInInputCSV - 1, extractedRecords.size());
+		
+	}
+
+	/**
+	 * Scenario : Positive 
+	 * scenario : Processing the input XML file and extracting
+	 * values as POJO object for validation process
+	 * @throws IOException 
+	 * @throws JAXBException 
+	 * @throws RaboBankStmtProcessException 
+	 */
+	@Test
+	public void extractStatmentFromXMLTestCase() throws IOException, RaboBankStmtProcessException, JAXBException {
+		ExtractorService extractorServiceImpl = new ExtractorServiceImpl();
+		File inputFile = new File(processStatementFileName + ".xml");
+		int totalLineInInputXML = (int) Files.lines(Paths.get(inputFile.getName())).count();
+		List<Record> extractedRecords = extractorServiceImpl.extractStatmentFromXML(inputFile);
+		assertEquals((totalLineInInputXML - 2) / 7, extractedRecords.size());
+	}
+
+}
--- src/test/java/com/rabobank/service/ValidatorServiceImplTest.java
+++ src/test/java/com/rabobank/service/ValidatorServiceImplTest.java
@@ -1,1 +1,79 @@
+package com.rabobank.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rabobank.model.Record;
+import com.rabobank.model.ResultRecord;
+
+@SpringBootTest
+public class ValidatorServiceImplTest {
+
+	/**
+	 * Scenario : Positive 
+	 * scenario : Duplicate check in given Rabobank Customer
+	 * Statement
+	 */
+	@Test
+	public void getDuplicateRecordsTestCaseWithDuplilcate() {
+		List<Record> inputList = Arrays.asList(
+				new Record(179430, "NL69ABNA0433647324", 66.72, -41.74, "Tickets for Willem Theuß", 24.98),
+				new Record(179430, "NL43AEGO0773393871", 16.52, +43.09, "Tickets for Willem Theuß", 59.61));
+		ValidatorService validatorServiceImpl = new ValidatorServiceImpl();
+		List<ResultRecord> duplicateRecords = validatorServiceImpl.getDuplicateRecordsByRef(inputList);
+		assertEquals(inputList.size(), duplicateRecords.size());
+
+	}
+
+	/**
+	 * Scenario : Negative 
+	 * scenario : Duplicate check in given Rabobank Customer
+	 * Statement
+	 */
+	@Test
+	public void getDuplicateRecordsTestCaseWithOutDuplilcate() {
+		List<Record> inputList = Arrays.asList(
+				new Record(172823, "NL69ABNA0433647324", 66.72, -41.74, "Tickets for Willem Theuß", 24.98),
+				new Record(179430, "NL43AEGO0773393871", 16.52, +43.09, "Tickets for Willem Theuß", 59.61));
+		ValidatorService validatorServiceImpl = new ValidatorServiceImpl();
+		List<ResultRecord> duplicateRecords = validatorServiceImpl.getDuplicateRecordsByRef(inputList);
+		assertEquals(0, duplicateRecords.size());
+
+	}
+
+	/**
+	 * Scenario : Positive 
+	 * scenario : EndBalance validation in given Rabobank Customer
+	 * Statement
+	 */
+	@Test
+	public void getEndBalanceErrorRecordsTestCaseWithWrongValue() {
+		List<Record> inputList = Arrays.asList(
+				new Record(179430, "NL69ABNA0433647324", 66.72, -41.74, "Tickets for Willem Theuß", 24.98),
+				new Record(179430, "NL43AEGO0773393871", 16.52, +43.09, "Tickets for Willem Theuß", 59.80));
+		ValidatorService validatorServiceImpl = new ValidatorServiceImpl();
+		List<ResultRecord> endBalanceErrorRecords = validatorServiceImpl.getEndBalanceErrorRecords(inputList);
+		assertEquals(inputList.size(), endBalanceErrorRecords.size());
+
+	}
+
+	/**
+	 * Scenario : Negative 
+	 * scenario : EndBalance validation in given Rabobank Customer
+	 * Statement
+	 */
+	@Test
+	public void getEndBalanceErrorRecordsTestCaseWithProperValue() {
+		List<Record> inputList = Arrays.asList(
+				new Record(179430, "NL69ABNA0433647324", 66.72, -41.74, "Tickets for Willem Theuß", 108.46),
+				new Record(179430, "NL43AEGO0773393871", 16.52, +43.09, "Tickets for Willem Theuß", -26.57));
+		ValidatorService validatorServiceImpl = new ValidatorServiceImpl();
+		List<ResultRecord> endBalanceErrorRecords = validatorServiceImpl.getEndBalanceErrorRecords(inputList);
+		assertEquals(0, endBalanceErrorRecords.size());
+	}
+}
