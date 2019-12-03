b70d8c627542c63c9e52d2d42fc1eb642b785f65
manickaselvi <57222412+manickaselvi@users.noreply.github.com>
2019-12-03 20:35:50 +0530

    Created test class for RaboBnkCustStmtProcessorApplicationTests

--- src/test/java/com/rabobank/RaboBnkCustStmtProcessorApplicationTests .java
+++ src/test/java/com/rabobank/RaboBnkCustStmtProcessorApplicationTests .java
@@ -1,1 +1,17 @@
+package com.rabobank;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author manickaselvi m
+ *
+ */
+@SpringBootTest
+class RaboBnkCustStmtProcessorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
