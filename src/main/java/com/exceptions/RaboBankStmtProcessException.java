c9e488193813d028e915221f453563af77896db6
manickaselvi <57222412+manickaselvi@users.noreply.github.com>
2019-12-03 19:17:45 +0530

    Created custom exceptions

---
+++ src/main/java/com/exceptions/RaboBankStmtProcessException.java
@@ -0,0 +1,29 @@
+
+/**
+ * @author manickaselvi.m
+ *
+ */
+public class RaboBankStmtProcessException extends Exception {
+
+	private static final long serialVersionUID = 7718828512143293558L;
+
+	public RaboBankStmtProcessException() {
+		super();
+	}
+
+	public RaboBankStmtProcessException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public RaboBankStmtProcessException(String message, Exception ex) {
+		super(message, ex);
+	}
+
+	public RaboBankStmtProcessException(Throwable cause) {
+		super(cause);
+	}
+
+	public RaboBankStmtProcessException(String message) {
+		super(message);
+	}
+}
