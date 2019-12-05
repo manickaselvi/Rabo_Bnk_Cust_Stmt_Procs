7d51b6e44d57f70239c5f23e2ade221186cac96c
manickaselvi <57222412+manickaselvi@users.noreply.github.com>
2019-12-05 14:53:25 +0530

    Sonar fixes done for ValidatorServiceImplTest.java

--- src/test/java/com/rabobank/service/ValidatorServiceImplTest.java
+++ src/test/java/com/rabobank/service/ValidatorServiceImplTest.java
@@ -14,6 +14,10 @@ import com.rabobank.model.ResultRecord;
 @SpringBootTest
 public class ValidatorServiceImplTest {
 
+	private static final String RECORD_DESCRIPTION = "Tickets for Willem Theuß";
+	private static final String RECORD_ACCT_NO_ONE = "NL43AEGO0773393871";
+	private static final String RECORD_ACCT_NO_TWO ="NL69ABNA0433647324";
+	
 	/**
 	 * Scenario : Positive 
 	 * scenario : Duplicate check in given Rabobank Customer
@@ -22,8 +26,8 @@ public class ValidatorServiceImplTest {
 	@Test
 	public void getDuplicateRecordsTestCaseWithDuplilcate() {
 		List<Record> inputList = Arrays.asList(
-				new Record(179430, "NL69ABNA0433647324", 66.72, -41.74, "Tickets for Willem Theuß", 24.98),
-				new Record(179430, "NL43AEGO0773393871", 16.52, +43.09, "Tickets for Willem Theuß", 59.61));
+				new Record(179430, RECORD_ACCT_NO_TWO, 66.72, -41.74, RECORD_DESCRIPTION, 24.98),
+				new Record(179430, RECORD_ACCT_NO_ONE, 16.52, +43.09, RECORD_DESCRIPTION, 59.61));
 		ValidatorService validatorServiceImpl = new ValidatorServiceImpl();
 		List<ResultRecord> duplicateRecords = validatorServiceImpl.getDuplicateRecordsByRef(inputList);
 		assertEquals(inputList.size(), duplicateRecords.size());
@@ -38,8 +42,8 @@ public class ValidatorServiceImplTest {
 	@Test
 	public void getDuplicateRecordsTestCaseWithOutDuplilcate() {
 		List<Record> inputList = Arrays.asList(
-				new Record(172823, "NL69ABNA0433647324", 66.72, -41.74, "Tickets for Willem Theuß", 24.98),
-				new Record(179430, "NL43AEGO0773393871", 16.52, +43.09, "Tickets for Willem Theuß", 59.61));
+				new Record(172823, RECORD_ACCT_NO_TWO, 66.72, -41.74, RECORD_DESCRIPTION, 24.98),
+				new Record(179430, RECORD_ACCT_NO_ONE, 16.52, +43.09, RECORD_DESCRIPTION, 59.61));
 		ValidatorService validatorServiceImpl = new ValidatorServiceImpl();
 		List<ResultRecord> duplicateRecords = validatorServiceImpl.getDuplicateRecordsByRef(inputList);
 		assertEquals(0, duplicateRecords.size());
@@ -54,8 +58,8 @@ public class ValidatorServiceImplTest {
 	@Test
 	public void getEndBalanceErrorRecordsTestCaseWithWrongValue() {
 		List<Record> inputList = Arrays.asList(
-				new Record(179430, "NL69ABNA0433647324", 66.72, -41.74, "Tickets for Willem Theuß", 24.98),
-				new Record(179430, "NL43AEGO0773393871", 16.52, +43.09, "Tickets for Willem Theuß", 59.80));
+				new Record(179430, RECORD_ACCT_NO_TWO, 66.72, -41.74, RECORD_DESCRIPTION, 24.98),
+				new Record(179430, RECORD_ACCT_NO_ONE, 16.52, +43.09, RECORD_DESCRIPTION, 59.80));
 		ValidatorService validatorServiceImpl = new ValidatorServiceImpl();
 		List<ResultRecord> endBalanceErrorRecords = validatorServiceImpl.getEndBalanceErrorRecords(inputList);
 		assertEquals(inputList.size(), endBalanceErrorRecords.size());
@@ -70,8 +74,8 @@ public class ValidatorServiceImplTest {
 	@Test
 	public void getEndBalanceErrorRecordsTestCaseWithProperValue() {
 		List<Record> inputList = Arrays.asList(
-				new Record(179430, "NL69ABNA0433647324", 66.72, -41.74, "Tickets for Willem Theuß", 108.46),
-				new Record(179430, "NL43AEGO0773393871", 16.52, +43.09, "Tickets for Willem Theuß", -26.57));
+				new Record(179430, RECORD_ACCT_NO_TWO, 66.72, -41.74, RECORD_DESCRIPTION, 108.46),
+				new Record(179430, RECORD_ACCT_NO_ONE, 16.52, +43.09, RECORD_DESCRIPTION, -26.57));
 		ValidatorService validatorServiceImpl = new ValidatorServiceImpl();
 		List<ResultRecord> endBalanceErrorRecords = validatorServiceImpl.getEndBalanceErrorRecords(inputList);
 		assertEquals(0, endBalanceErrorRecords.size());
