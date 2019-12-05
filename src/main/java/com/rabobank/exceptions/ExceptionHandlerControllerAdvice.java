505a6a6d424ba39d0dfa7a54c17ef3267057057f
manickaselvi <57222412+manickaselvi@users.noreply.github.com>
2019-12-05 19:42:26 +0530

    Exception handled through Controller Advice

---
+++ src/main/java/com/rabobank/exceptions/ExceptionHandlerControllerAdvice.java
@@ -0,0 +1,51 @@
+package com.rabobank.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.rabobank.constants.RBStatementProcessConstants;
+import com.rabobank.model.StatementProcessResponse;
+
+/**
+ * @author manickaselvi.m
+ *
+ */
+@ControllerAdvice
+public class ExceptionHandlerControllerAdvice {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	/**
+	 * @param exception
+	 * @param req
+	 * @return
+	 */
+	@ExceptionHandler(RaboBankStmtProcessException.class)
+	public @ResponseBody ResponseEntity<StatementProcessResponse> handleStatementProcessExp(
+			final RaboBankStmtProcessException exception, HttpServletRequest req) {
+		logger.info("ALERT-->>Internal Server Error");
+		return new ResponseEntity<>(
+				new StatementProcessResponse(exception.getMessage(), RBStatementProcessConstants.HTTP_CODE_ERROR, null),
+				HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	/**
+	 * @return
+	 */
+	@ExceptionHandler(Exception.class)
+	@ResponseBody
+	public ResponseEntity<StatementProcessResponse> handleExceptions() {
+		logger.info("ALERT-->>Internal Server Error");
+		return new ResponseEntity<>(
+				new StatementProcessResponse(RBStatementProcessConstants.UNEXPECTED_SERVER_ERROR,
+						RBStatementProcessConstants.HTTP_CODE_ERROR, null),
+				HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
