658bc5114192b40cad48f66f511ed1eb469270d8
manickaselvi <57222412+manickaselvi@users.noreply.github.com>
2019-12-05 12:51:45 +0530

    Sonar fix for RBStatementProcessConstants.java

--- src/main/java/com/rabobank/constants/RBStatementProcessConstants.java
+++ src/main/java/com/rabobank/constants/RBStatementProcessConstants.java
@@ -6,6 +6,7 @@ package com.rabobank.constants;
  */
 public class RBStatementProcessConstants {
 	
+	private RBStatementProcessConstants(){}
 	public static final Integer HTTP_CODE_SUCCESS = 200;
 	public static final Integer HTTP_CODE_ERROR = 500;
 	public static final Integer HTTP_CODE_INVALID_INPUT = 406;
